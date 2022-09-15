e5e7343b29fa62f26ce12ad631c6a2c672bd566b
Marina <marina.zilkina@gmail.com>
2022-09-15 15:42:42 +0300

    Добавила детальны вывод сообщений

--- TheBoard/GameBoard/views.py
+++ TheBoard/GameBoard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .forms import PostForm
 from .models import Post
@@ -12,7 +12,10 @@ class PostList(ListView):
     context_object_name = 'posts'
 
 
-
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
 
     '''def image_upload_view(request):
         if request.method == 'POST':
